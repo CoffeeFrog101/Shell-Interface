77fcecd8b459c78859cf8c7ea18419002c85118d
t0170536 <t0170536@my.ntu.ac.uk>
2022-04-03 19:31:00 +0100

    final program just needed some tweaks

--- src/main/java/basicCommands.java
+++ src/main/java/basicCommands.java
@@ -2,27 +2,22 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 public class basicCommands {
 
     public static void main(String[] args) throws IOException {
-        List<String> BuiltIn = new ArrayList<>();
-        BuiltIn.add("help");
-        BuiltIn.add("showDir");
-
 
 
 
         File dir = new File(System.getProperty("user.home"));
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ProcessBuilder PB = new ProcessBuilder(BuiltIn);
+        ProcessBuilder PB = new ProcessBuilder();
         PB.directory(dir);
 
         String CommandLine;
-
 
 
         while (true) {
@@ -31,15 +26,13 @@ public class basicCommands {
             System.out.print(dir + " Prompt$ ");
             CommandLine = br.readLine();
             String[] mycommands = CommandLine.split(" ");//stores all commands
-            if (CommandLine.equals("")){
+            if (CommandLine.equals("")) {// so it can continue when the user press enter
                 continue;
             }
-            try{
-               // BuiltINs BuiltINsobject = new BuiltINs();
-                //
-                // BuiltINsobject.builtin();
+            try {
 
-                if(CommandLine.contains("quit")){
+
+                if (CommandLine.contains("quit")) {
                     System.exit(0);
 
                 }
@@ -56,12 +49,45 @@ public class basicCommands {
                     System.out.println("move --Copy file from src to target \n");
                     System.out.println("cd --Change the current directory \n");
                 }
+                if (CommandLine.contains("showDir")) {
+                    File current = new File(System.getProperty("user.dir"));
+                    PB.directory(current);
+                    System.out.println(current);
+
+
+                }
+                if(CommandLine.contains("move")){
+                    File mv = new File("/Users/ziadahmed/System-software/src/main/java");
+
+                    // renaming the file and moving it to a new location
+                    if(mv.renameTo
+                            (new File("/Users/ziadahmed/System-software/target")))
+                    {
+
+                        mv.delete();
+                        System.out.println("File moved successfully");
+                    }
+                    else
+                    {
+                        System.out.println("Failed to move the file");
+                    }
+
+
+                }
+
+                if (CommandLine.contains("whoAmI")) {
+
+                    String name;
+                    name = System.getProperty("user.name");
+                    System.out.println(name);
+                }
 
 
 
-                PB.command(BuiltIn);
+
+
                 PB.command(mycommands);
-                // all commands typed will be
+                // all commands typed will be stored in an array
                 Process pro = PB.start();
                 BufferedReader bro = new BufferedReader(new InputStreamReader(pro.getInputStream()));
 
@@ -69,9 +95,9 @@ public class basicCommands {
                 while ((l = bro.readLine()) != null) {
                     System.out.println(l);
                 }
+
                 bro.close();
-            }
-            catch (Exception l){
+            } catch (Exception l) {
                 System.out.println(l.getMessage());
 
             }
@@ -79,62 +105,5 @@ public class basicCommands {
         }
 
 
-
     }
-//BuiltIn Commands
-    public void builtin()throws IOException {
-        File dir2 = new File(System.getProperty("user.home"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String CommandLine;
-
-
-        while (true) {
-
-
-            System.out.print(dir2 + " Prompt$ ");
-            CommandLine = br.readLine();
-
-            if (CommandLine.equals("")) {
-                continue;
-            }
-
-
-            if (CommandLine.contains("help")) {
-                System.out.println("ls --List the current directory \n");
-                System.out.println("mkdir --Creates a new directory\n ");
-                System.out.println("rmdir --Removes a directory\n ");
-                System.out.println("ps -Report a snapshot of the current processes \n");
-                System.out.println("which --List the current directory \n");
-                System.out.println("mv --Lists the current directory \n");
-                System.out.println("help --shows the commands\n");
-                System.out.println("showDir --Show the current directory \n");
-                System.out.println("whoAmI -Shows the current username \n");
-                System.out.println("move --Copy file from src to target \n");
-                System.out.println("cd --Change the current directory \n");
-            }
-
-            if (CommandLine.contains("showDir")) {
-                System.out.println(dir2);
-
-            }
-
-            if(CommandLine.contains("whoAmI")){
-                String name;
-                name = System.getProperty("user.name");
-                System.out.println(name);
-            }
-
-            if(CommandLine.contains("cd")){
-
-            }
-
-
-        }
-
-
-    }
-
 }
-
-
-
